d597ab9868a1cad745ae73a4f1822d83385d512b
Mehdi Behrooz <mehdi.behrooz@gmail.com>
2014-11-15 05:46:00 +0330

    supporting auto direction

--- source/rtl.js
+++ source/rtl.js
@@ -1,2 +1,21 @@
 // make message input RTL
 document.getElementById('message-input').dir = 'auto';
+
+// make all directions auto
+
+function fix_directions() {
+  $('.message_content').each(function() {
+    $(this).attr('dir', 'auto');
+    $(this).html($.trim($(this).html()));
+  });
+}
+
+function bind(){
+    $(this).unbind('DOMSubtreeModified.event1');
+    setTimeout(function(){
+        fix_directions();
+        $('body').bind('DOMSubtreeModified.event1',DOMModificationHandler);
+    },1000);
+}
+
+$('body').bind('DOMSubtreeModified.event1', bind);
